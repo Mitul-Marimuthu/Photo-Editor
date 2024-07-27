321b43b3f44eae3358fef261c0873e0ea6a469c5
Mitul-Marimuthu <mitul6m16@gmail.com>
2024-07-27 15:01:42 -0700

    first

---
+++ app/(root)/transformations/page/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+  return (
+    <div>TransformationsPage</div>
+  )
+}
+
+export default TransformationsPage
+
+// localhost:300/tranformations
+// localhose:300/tranformations/[id]
